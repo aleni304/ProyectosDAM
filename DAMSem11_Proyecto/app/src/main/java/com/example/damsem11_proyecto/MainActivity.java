a78dbfc9a8a6d485927b477d1a4673afe801fd71
Alexia Asuncion <alexiaap_304@hotmail.com>
2024-06-26 12:47:07 -0500

    Semana13-Notas

--- DAMSem11_Proyecto/app/src/main/java/com/example/damsem11_proyecto/MainActivity.java
+++ DAMSem11_Proyecto/app/src/main/java/com/example/damsem11_proyecto/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.damsem11_proyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Definir el evento click sobre el botón Login
+        btnLogin = findViewById(R.id.buttonLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NotasActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ DAMSem11_Proyecto/app/src/main/java/com/example/damsem11_proyecto/NotasInteractionListener.java
@@ -0,0 +1,7 @@
+package com.example.damsem11_proyecto;
+
+public interface NotasInteractionListener {
+    void editNotaClick(Nota nota);
+    void eliminaNotaClick (Nota nota);
+    void favoritaNotaClick (Nota nota);
+}
